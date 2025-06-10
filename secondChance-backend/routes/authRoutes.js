76f9b79a98b43306b022d677b13b3f489965fc31
svargasm93 <sevarmo@hotmail.com>
2025-06-09 21:31:57 -0400

    Develop Back-end Integration for Login

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -1,53 +1,104 @@
-const express = require('express')
-const bcryptjs = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const connectToDatabase = require('../models/db')
-const router = express.Router()
-const dotenv = require('dotenv')
-const pino = require('pino')  // Import Pino logger
-dotenv.config()
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const connectToDatabase = require('../models/db');
+const router = express.Router();
+const dotenv = require('dotenv');
+const pino = require('pino');  // Import Pino logger
+dotenv.config();
 
-const logger = pino()  // Create a Pino logger instance
+const logger = pino();  // Create a Pino logger instance
 
 //Create JWT secret
-const JWT_SECRET = process.env.JWT_SECRET
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
-  try {  
-    //Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
-    const db = await connectToDatabase()
-    const collection = db.collection("users")
-    const existingEmail = await collection.findOne({ email: req.body.email })
+    try {
+      //Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+      const db = await connectToDatabase();
 
-    if (existingEmail) {
-      logger.error('Email id already exists')
-      return res.status(400).json({ error: 'Email id already exists' })
+      //Access the `users` collection
+      const collection = db.collection("users");
+
+      //Check for existing email in DB
+      const existingEmail = await collection.findOne({ email: req.body.email });
+
+        if (existingEmail) {
+            logger.error('Email id already exists');
+            return res.status(400).json({ error: 'Email id already exists' });
+        }
+
+        const salt = await bcryptjs.genSalt(10);
+        const hash = await bcryptjs.hash(req.body.password, salt);
+        const email=req.body.email;
+
+        //Save user details
+        const newUser = await collection.insertOne({
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: hash,
+            createdAt: new Date(),
+        });
+
+        const payload = {
+            user: {
+                id: newUser.insertedId,
+            },
+        };
+
+        //Create JWT
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        logger.info('User registered successfully');
+        res.json({ authtoken,email });
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).send('Internal server error');
     }
+});
 
-    const salt = await bcryptjs.genSalt(10)
-    const hash = await bcryptjs.hash(req.body.password, salt)
-    const email=req.body.email
-    const newUser = await collection.insertOne({
-      email: req.body.email,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      password: hash,
-      createdAt: new Date(),
-    })
+    //Login Endpoint
+router.post('/login', async (req, res) => {
+    console.log("\n\n Inside login")
 
-    const payload = {
-      user: {
-        id: newUser.insertedId,
-      },
-    }
+    try {
+        // connect to `secondChance` in MongoDB through `connectToDatabase`
+        const db = await connectToDatabase();
+        //Access MongoDB `users` collection
+        const collection = db.collection("users");
+        //Check for user credentials in database
+        const theUser = await collection.findOne({ email: req.body.email });
+        //Check if the password matches
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            //send appropriate message if mismatch
+            if(!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong pasword' });
+            }
+            //Fetch user details
+            let payload = {
+                user: {
+                    id: theUser._id.toString(),
+                },
+            };
 
-    const authtoken = jwt.sign(payload, JWT_SECRET)
-    logger.info('User registered successfully')
-    res.json({ authtoken,email })
-  } catch (e) {
-    logger.error(e)
-    return res.status(500).send('Internal server error')
-  }
-})
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+            //Create JWT authentication if passwords match
+            const authtoken = jwt.sign(payload, JWT_SECRET);
+            logger.info('User logged in successfully');
+            return res.status(200).json({ authtoken, userName, userEmail });
+        //Send appropriate message if user not found
+        } else {
+            logger.error('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).json({ error: 'Internal server error', details: e.message });
+      }
+});
 
-module.exports = router
+module.exports = router;
